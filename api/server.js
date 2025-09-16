47388d1afce9f88d4724119af498d9828c66241e
Steven Corrales <scorrales18@gmail.com>
2025-09-16 14:58:23 -0600

    Version 1.1.1

--- api/server.js
+++ api/server.js
@@ -7,7 +7,7 @@ import connectDB from './config/db.js';
 import productRoutes from './routes/productRoutes.js';
 import authRoutes from './routes/authRoutes.js';
 import pdfRoutes from './routes/pdfRoutes.js';
-import historyRoutes from './routes/historyroutes.js';
+import historyRoutes from './routes/historyRoutes.js';
 
 dotenv.config();
 
